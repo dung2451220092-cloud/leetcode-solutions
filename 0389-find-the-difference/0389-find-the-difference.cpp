569a9b67e68d95ddc29363932424f0c08667c2b8
dung2451220092-cloud <dung_2451220092@dau.edu.vn>
2026-05-14 12:42:57 +0700

    Time: 0 ms (100%), Space: 9.1 MB (95.57%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,17 @@
+//count both diff will be extra 
+//sc O(1)
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+       int count1 = 0;
+       int count2 = 0;
+       for(int i=0;i<s.size();i++){
+         count1 += s[i]-'a';
+       }     
+        for(int i=0;i<t.size();i++){
+         count2 += t[i]-'a';
+       }   
+
+       return 'a' + (count2-count1);
+    }
+};
